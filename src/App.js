ccee651ef085d4e70b12a279c455b488a97b9cb0
Christopher Garner <125310823+cgarner90@users.noreply.github.com>
2023-05-23 15:07:52 -0500

    Random Quote Generator

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Quotes from './components/Quotes';
+import NameTag from './components/NameTag'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Quotes />
+      <NameTag />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NameTag.js
@@ -0,0 +1,9 @@
+import './NameTag.css'
+
+function NameTag () {
+    return (
+        <div id="nametag">designed by G.</div>
+    );
+}
+
+export default NameTag
